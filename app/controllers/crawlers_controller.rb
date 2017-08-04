fb0e12b96fc52a3a407fd7fe836badc276460ef4
zhangjingqiang <zhangjingqiang@gmail.com>
2017-08-04 12:44:08 +0900

    Change people to framework for default keyword

--- app/controllers/crawlers_controller.rb
+++ app/controllers/crawlers_controller.rb
@@ -1,6 +1,6 @@
 class CrawlersController < ApplicationController
   before_action :set_keyword
-  
+
   def google
     escaped_url = URI.escape("https://www.google.com/search?q=#{@keyword}&oe=utf-8&hl=en")
     @doc = Nokogiri::HTML(open(escaped_url))
@@ -17,7 +17,7 @@ class CrawlersController < ApplicationController
   def twitter
     @doc = Nokogiri.HTML(open("https://twitter.com/search?f=realtime&q=#{URI.encode(@keyword)}&src=typd"))
   end
-  
+
   def flickr
     @doc = Nokogiri::HTML(open("https://www.flickr.com/search/?q=#{URI.encode(@keyword)}"))
   end
@@ -37,7 +37,7 @@ class CrawlersController < ApplicationController
   def amazon
     url= "http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=#{URI.encode(@keyword)}"
     doc = Nokogiri::HTML(open(url))
-    
+
     @doc = []
     doc.xpath("//h3[@class='newaps']/a").each {|item|
       asin = item[:href].match(%r{dp/(.+)})[1]
@@ -45,10 +45,10 @@ class CrawlersController < ApplicationController
       @doc << Nokogiri::HTML(open(url)) rescue nil
     }
   end
-  
+
   private
-  
+
   def set_keyword
-    @keyword = (!session['keyword'].nil? && session['keyword'] != '')? session['keyword'] : 'barackobama'
+    @keyword = (!session['keyword'].nil? && session['keyword'] != '')? session['keyword'] : 'rails'
   end
 end
